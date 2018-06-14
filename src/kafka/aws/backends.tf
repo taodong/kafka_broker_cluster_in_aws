8a8df4c0d1b204ca749aa4f487d0cef753666a44
tao.dong <taojdcn@gmail.com>
2018-06-13 21:59:24 -0700

    Terraform variable update
    Update bucket names

--- src/kafka/aws/backends.tf
+++ src/kafka/aws/backends.tf
@@ -1,7 +1,7 @@
 terraform = {
   backend "s3" {
-    bucket = "veevacrm-mc-dt-temp"
-    key = "mc-dt/us-west-1/tao/terraform.tfstate"
+    bucket = "tao-terraform-state-bucket"
+    key = "kafka/us-west-1/tao/terraform.tfstate"
     region = "us-west-1"
     dynamodb_table = "terraform_locks-test4"
     encrypt = true
